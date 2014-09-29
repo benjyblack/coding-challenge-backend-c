ea0a690199c95f4823abd2ae5e18826e3d380998
BenBlack <benjyblack@gmail.com>
2014-09-28 22:29:39 -0400

    add formatting for output

--- data.js
+++ data.js
@@ -35,9 +35,42 @@ module.exports.getSimilarRecords = function(input) {
 
 	_(records).forEach(function(record, index) {
 		if (record.name.indexOf(input) != -1) {
-			similarRecords.push(record);
+			
+			similarRecords.push({
+				name: getFullLocationName(record),
+				latitude: record.lat,
+				longitude: record.long,
+				score: 0
+			});
 		}
 	});
 
 	return similarRecords;
+}
+
+var getFullLocationName = function(record) {
+	var country = record.country === 'US' ? 'USA' : 'Canada';
+	var admin1 = record.country === 'US' ? record.admin1 : fipsToProvince(record.admin1);
+
+	return record.name + ', ' + admin1 + ', ' + country;
+}
+
+var fipsToProvince = function(code) {
+	switch(+code) {
+		case 1: return "AB";
+		case 2: return "BC";
+		case 3: return "MB";
+		case 4: return "NB";
+		case 5: return "NL";
+		case 7: return "NS";
+		case 8: return "ON";
+		case 9: return "PE";
+		case 10: return "QC";
+		case 11: return "SK";
+		case 12: return "YT";
+		case 13: return "NT";
+		case 14: return "NU";
+	}
+
+	return "Invalid code";
 }
